eec89ebbc303fedcbc221510917a0604cdec6184
Arcadia Parson <aparson@catalyte.io>
2022-08-22 12:32:43 +1000

    got computer choice being randomly generated and displayed

--- app.js
+++ app.js
@@ -2,21 +2,33 @@ const computerChoiceDisplay = document.getElementById('computer-choice')
 const playerChoiceDisplay = document.getElementById('player-choice')
 const resultDisplay = document.getElementById('result')
 
-const possibleChoices = document.querySelectorAll('button');
-
-
-
-let playerChoice
+const possibleChoices = document.getElementsByName('choiceButton');
+console.log(possibleChoices);
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
     playerChoice = e.target.id
     playerChoiceDisplay.innerHTML = playerChoice
-    // generateComputerChoice()
+    generateComputerChoice()
 }))
 
-// const generateComputerChoice = () => {
-//     const randomNumber = Math.floor(Math.random(1) * possibleChoices.length);
-//     console.log(randomNumber);
-
-// }
+const generateComputerChoice = () => {
+    const randomNumber = Math.floor(Math.random() * possibleChoices.length);
+    switch (randomNumber) {
+        case 0: 
+            computerChoiceDisplay = "rock";
+            break;
+        case 1: 
+            computerChoiceDisplay = "paper";
+            break;
+        case 2: 
+            computerChoiceDisplay = "scissors";
+            break;
+        case 3:
+            computerChoiceDisplay = "lizard";
+            break;
+        case 4:
+            computerChoiceDisplay = "spock";
+            break;
+    }
+}
 
