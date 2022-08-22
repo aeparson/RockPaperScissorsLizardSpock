8ffb37f7c828d5d10453589e289563ebb8730283
Arcadia Parson <aparson@catalyte.io>
2022-08-22 12:02:38 +1000

    fixed possible choces

--- app.js
+++ app.js
@@ -2,12 +2,7 @@ const computerChoiceDisplay = document.getElementById('computer-choice')
 const userChoiceDisplay = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('result')
 
-const possibleChoices = document.qgetElementsByClassName('button')
-let userChoice
+const possibleChoices = document.getElementsByClassName('choiceButton');
 
-possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (event) => {
-    userChoice = event.target.id
-    userChoiceDisplay.innerHTML = userChoice
-    generateComputerChoice()
-}))
+
 
