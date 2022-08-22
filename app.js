45c2760db0569dabbae30154f719c38003d72985
Arcadia Parson <aparson@catalyte.io>
2022-08-22 12:12:28 +1000

    user choice working

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const computerChoiceDisplay = document.getElementById('computer-choice')
-const userChoiceDisplay = document.getElementById('user-choice')
+const playerChoiceDisplay = document.getElementById('player-choice')
 const resultDisplay = document.getElementById('result')
 
-const possibleChoices = document.getElementsByClassName('choiceButton');
+const possibleChoices = document.querySelectorAll('button');
 
 
+
+let playerChoice
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    playerChoice = e.target.id
+    playerChoiceDisplay.innerHTML = playerChoice
+    // generateComputerChoice()
+}))
+
+// const generateComputerChoice = () => {
+//     const randomNumber = Math.floor(Math.random(1) * possibleChoices.length);
+//     console.log(randomNumber);
+
+// }
 
